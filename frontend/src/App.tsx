849817ac9edfb32c6d2675f558f51dfa84164995
PauloJsReact <paulo_henrique_araujo@outlook.com>
2021-11-03 19:22:27 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import DataTable from 'Components/DataTable';
+import Footer from 'Components/Footer';
+import NavBar from 'Components/NavBar';
 import React from 'react';
 
 
 function App() {
   return (
-    <div className="App">
-     <h1 className="text-primary">Hello word</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Ola mundo</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
