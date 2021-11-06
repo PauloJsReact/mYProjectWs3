998bc637e344eb9537469a7be30cc33f77c13068
PauloJsReact <paulo_henrique_araujo@outlook.com>
2021-11-06 19:55:35 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,11 @@
-import BarChart from 'Components/BarChart';
-import DataTable from 'Components/DataTable';
-import DonutChart from 'Components/DonutChart';
-import Footer from 'Components/Footer';
-import NavBar from 'Components/NavBar';
+
 import React from 'react';
+import Routes from 'Routes';
 
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-        <div className="row  px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso  (%)</h5>
-            <BarChart />
-          </div>
-
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso  (%)</h5>
-            <DonutChart />
-          </div>
-
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Todas vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-
-      <Footer />
-    </>
+   <Routes />
   );
 }
 
